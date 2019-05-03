773afff311bb3ec2757120c59a03413a012ab043
anamipita <anamipita@gmail.com>
2019-05-03 14:49:30 +0300

    app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            ksho
+            i was changed in feature
           </p>
           <a
             className="App-link"
